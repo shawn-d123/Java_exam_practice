cd0b6d7b73dc1bf4548d3dd2f0a25d48365c4fd8
Shawn Dsouza <dsouzashawn305@gmail.com>
2026-02-11 20:36:28 +0000

    exam coding practice

--- src/versionControl_test.java
+++ src/versionControl_test.java
@@ -1,5 +1,10 @@
 public class versionControl_test {
     public static void main(String[] args) {
         System.out.println("Version Control Test");
+
+        // test works well
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Test " + (i + 1));
+        }
     }
 }
