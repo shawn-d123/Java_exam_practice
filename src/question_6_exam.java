a1a5c7f68648e98b27996764d7103557ad26eb90
Shawn Dsouza <dsouzashawn305@gmail.com>
2026-02-12 18:48:08 +0000

    exam coding practice

--- src/question_6_exam.java
+++ src/question_6_exam.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class question_6_exam {
 
     public static void main(String[] args) {
@@ -6,10 +8,26 @@ public class question_6_exam {
 
     public static double[] PredictWeather(double[] T, int xdays) {
 
+        // test  1
+        if (T == null || T.length == 0) {
+            double[] test1 = {-1.0};
+            return test1;
+        }
+        // test 2
+        if(xdays < 1 ){
+            double[] test2 = {-2.0};
+            return test2;
+        }
+
         double total = 0;
         // step 1
         for (int i = 0; i < T.length; i++) {
 
+            // test 3
+            if (T[i] < -89.2 || T[i] > 56.7){
+                double[] test3 = {-3.0};
+                return test3;
+            }
             // handle rest of values
             if(i > 0 && i < T.length-1){
                 total += T[i + 1] - T[i];
@@ -23,8 +41,10 @@ public class question_6_exam {
 
         for (int i = 0; i < P.length; i++) {
 
+            P[i] = T[T.length - 1] + (total * (i + 1) );
         }
+        System.out.println("P is: " + Arrays.toString(P));
 
-        return T;
+        return P;
     }
 }
