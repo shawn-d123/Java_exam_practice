8299e38eacbb65ca38f22d85a9f7690da393b95e
Shawn Dsouza <dsouzashawn305@gmail.com>
2026-02-12 18:37:52 +0000

    exam coding practice

--- src/question_6_exam.java
+++ src/question_6_exam.java
@@ -1,3 +1,24 @@
 public class question_6_exam {
 
+    public static void main(String[] args) {
+        double[] T = {14.5, 15.2, 16.0, 15.8, 17.1, 18.3, 19.0};
+    }
+
+    public static double[] PredictWeather(double[] T, int xdays) {
+
+        double total = 0;
+        // step 1
+        for (int i = 0; i < T.length; i++) {
+
+            // handle rest of values
+            if(i > 0 && i < T.length-1){
+                total += T[i + 1] - T[i];
+            }
+
+        }
+
+        // step 2
+        Double
+        return T;
+    }
 }
