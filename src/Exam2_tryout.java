01047b2dd8cb7d507ecfd894acaec3e6d7f5da9f
Shawn Dsouza <dsouzashawn305@gmail.com>
2026-04-12 20:50:43 +0100

    exam coding practice

--- src/Exam2_tryout.java
+++ src/Exam2_tryout.java
@@ -18,7 +18,6 @@ public class Exam2_tryout {
                     System.out.println(i);
                 }
             }
-
             System.out.println("=====================");
         }
     }
