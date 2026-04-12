8d1de2cac4397709a6bf3bd1b14b93e29ca5db78
Shawn Dsouza <dsouzashawn305@gmail.com>
2026-04-12 20:48:52 +0100

    exam coding practice

--- src/Exam2_tryout.java
+++ src/Exam2_tryout.java
@@ -5,6 +5,20 @@ public class Exam2_tryout {
         for(int i = 0; i <= 10; i++){
             System.out.println(i);
         }
+
+        boolean isrunning = true;
+        while(isrunning == true){
+            System.out.println("Hello World!");
+
+            for (int i = 0; i <= 10; i++){
+                if (i == 5){
+                    isrunning = false;
+                }
+                else{
+                    System.out.println(i);
+                }
+            }
+        }
     }
 
 
